97ecb2a8cd04675540a450e90610645ba407535a
Ara <hello.ara@pm.me>
2021-02-12 07:18:33 +0000

    delete unused files

--- Makers/Projects/week9/bank-tech-test/spec/date.js
+++
@@ -1,9 +0,0 @@
-describe('Date', () => {
-  it('should return dates date', () => {
-    let date = new DateClass();
-    spyOn(date, "getMonth").and.returnValue("02")
-    spyOn(date, "getDate").and.returnValue("11")
-    spyOn(date, "getYear").and.returnValue("2021")
-    expect(date.display()).toEqual("11/02/2021")
-  })
-})
